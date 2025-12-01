cdb8ed2094a319b8cda9570967fa906648caa5f0
Bostjan Tucic <info@t-bit.si>
2025-12-01 11:25:20 +0100

    AoC 2025 - day 1, 1. task

---
+++ Day1/Day1_1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System;
+
+
+string filePath = "input.txt"; //correct password for this input is 1102
+//string filePath = "test.txt"; //correct password for this input is 1102
+int password = 0;
+int dialPosition = 50; //starting position is 3
+
+if (File.Exists(filePath))
+{
+    try
+    {
+        foreach (string line in File.ReadLines(filePath))
+        {
+            char firstChar = line[0];
+            if (firstChar != 'L' && firstChar != 'R')
+            {
+                throw new Exception("Error, Unexpected rotation sign input");
+            }
+
+            string numberString = line.Substring(1);
+            int parsedNumber;
+            bool parsingOk = int.TryParse(numberString, out parsedNumber);
+
+            if (!parsingOk)
+            {
+                throw new Exception("Error, Unexpected number input");
+            }
+
+            while (parsedNumber >= 100)
+            {
+                parsedNumber -= 100;
+            }
+
+            if (firstChar == 'L')
+            {
+                dialPosition -= parsedNumber;
+                if (dialPosition < 0) { dialPosition += 100; }
+            }
+            else
+            {
+                dialPosition += parsedNumber;
+                if (dialPosition > 99) { dialPosition -= 100; }
+            }
+
+            if (dialPosition == 0)
+            {
+                password++;
+            }
+        }
+    }
+    catch (IOException ex)
+    {
+        Console.WriteLine($"IOException: {ex.Message}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"General exception: {ex.Message}");
+    }
+}
+else
+{
+    Console.WriteLine($"File dosn't exists: {filePath}");
+
+}
+
+Console.WriteLine("Pasword is {0}", password);
+
