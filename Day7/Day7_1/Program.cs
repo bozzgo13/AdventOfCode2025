7068fa53b81418bcdb51d3734e45aac0c20eae5a
Bostjan Tucic <info@t-bit.si>
2025-12-17 16:18:39 +0100

    AoC 2025 - day 7, 1. task

---
+++ Day7/Day7_1/Program.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Numerics;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        string inputFileName = "input.txt"; // Beam will be split 1635 times!
+
+        var data = File.ReadAllLines(inputFileName);
+
+        var firstLine = data[0];
+
+        Console.WriteLine(firstLine);
+        int startingPoint = firstLine.IndexOf("S");
+        int beamSplitCount = 0;
+
+        SortedSet<int> positions = new SortedSet<int>();
+        positions.Add(startingPoint);
+
+        for (int index = 1; index < data.Length; index++)
+        {
+            var nextLine = data[index];
+
+            if (index % 2==0) //spliting 
+            {
+                StringBuilder sb = new StringBuilder(nextLine);
+
+                List<int> removeIndexes = new List<int>();
+                List<int> addIndexes = new List<int>();
+
+
+                foreach (var item in positions)
+                {
+                    if (nextLine[item].Equals('^'))
+                    {
+                        beamSplitCount++;
+                        removeIndexes.Add(item);
+                        sb[item-1] = '|';
+                        sb[item+1] = '|';
+                        addIndexes.Add(item-1);
+                        addIndexes.Add(item+1);
+                    }
+                    else
+                    {
+                        sb[item] = '|';
+                    }
+                }
+
+                foreach (var item in removeIndexes)
+                {
+                    positions.Remove(item);
+                }
+                foreach (var item in addIndexes)
+                {
+                    positions.Add(item);
+                }
+
+                nextLine = sb.ToString();
+
+            }
+            else 
+            {
+                StringBuilder sb = new StringBuilder(nextLine);
+                foreach (var item in positions)
+                {
+                    sb[item] = '|';
+                }
+
+                nextLine = sb.ToString();
+
+            }
+            Console.WriteLine(nextLine);
+           
+        }
+        Console.WriteLine($"Beam will be split {beamSplitCount} times!");
+
+    }
+
+}
