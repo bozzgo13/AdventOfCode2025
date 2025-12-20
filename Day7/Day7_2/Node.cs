e10a886673f9260fa1eaf079918cd9e87a29cb6f
Bostjan Tucic <info@t-bit.si>
2025-12-20 16:55:30 +0100

    AoC 2025 - day 7, 2. task

---
+++ Day7/Day7_2/Node.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_2
+{
+    public abstract class Node
+    {
+        public Node? Prev { get; set; }
+        public Position Pos { get; set; }
+        public ParentPosition ParentPos { get; set; }
+
+        public int Depth { get; set; }
+
+
+        public Node(Node? prev, Position pos, ParentPosition parentPos)
+        {
+            Prev = prev;
+            Pos = pos;
+            ParentPos = parentPos;
+        }
+    }
+
+    public class Position
+    {
+
+        public int row, col;
+
+        public Position(int r, int c)
+        {
+            row = r; col = c;
+        }
+    }
+
+    public class LinearNode : Node
+    {
+        public LinearNode(Node? prev, Position pos, ParentPosition parentPos) : base(prev, pos, parentPos)
+        {
+        }
+
+        public Node? Next { get; set; }
+    }
+
+    public class ForkNode : Node
+    {
+        public ForkNode(Node? prev, Position pos, ParentPosition parentPos) : base(prev, pos, parentPos)
+        {
+        }
+
+        public Node? NextLeft { get; set; }
+        public Node? NextRight { get; set; }
+    }
+
+    public enum ParentPosition
+    {
+        Left,
+        Right,
+        Linear,
+        None
+    }
+}
---
+++ Day7/Day7_2/Program.cs
@@ -0,0 +1,143 @@
+﻿using Day7_2;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq.Expressions;
+using System.Numerics; 
+
+class Program
+{
+    static void Main()
+    {
+        var lines = File.ReadAllLines("input.txt");
+
+        // var lines = new string[] {
+        //".......S.......",
+        //"...............",
+        //".......^.......",
+        //"...............",
+        //"......^.^......",
+        //"...............",
+        //".....^.^.^.....",
+        //"...............",
+        //"....^.^...^....",
+        //"...............",
+        //"...^.^...^.^...",
+        //"...............",
+        //"..^...^.....^..",
+        //"...............",
+        //".^.^.^.^.^...^.",
+        //"...............",
+        //    };
+        int cols = lines[0].Length;
+        int rows = lines.Length;
+        char[,] grid = new char[rows, cols];
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                grid[r, c] = lines[r][c];
+            }
+        }
+        
+        int startinPos = lines[0].IndexOf("S");
+        int rowPosition = 0;
+
+
+        Node n = new LinearNode(null, new Position(rowPosition, startinPos), ParentPosition.None);
+        n.Depth = 0;
+        Node startingNode = n;
+        BigInteger counter = BigInteger.Zero;
+        bool isEnd = false;
+        while (!isEnd)
+        {
+
+            if (n.Pos.row == rows - 1)
+            {
+                counter++;
+
+                if (n.ParentPos == ParentPosition.Left)
+                {
+                    ForkNode p = n.Prev as ForkNode;
+                    n = p.NextRight;
+                    continue;
+                }
+                if (n.ParentPos == ParentPosition.Right || n.ParentPos == ParentPosition.Linear)
+                {
+                    ForkNode p = null;
+                    while (n.ParentPos != ParentPosition.None && n.ParentPos != ParentPosition.Left)
+                    {
+                        n = n.Prev;
+
+                    }
+                    if (n.ParentPos == ParentPosition.Left)
+                    {
+                        p = n.Prev as ForkNode;
+                        p.NextLeft = null;
+
+                        n = p.NextRight;
+                        continue;
+                    }
+                    else if (n.ParentPos == ParentPosition.None)
+                    {
+                        isEnd = true;
+                    }
+
+                    continue;
+                }
+
+            }
+
+            if (n is LinearNode linear)
+            {
+                if (grid[n.Pos.row + 1, n.Pos.col].Equals('.'))
+                {
+                    Position pos = new Position(n.Pos.row + 1, n.Pos.col);
+                    LinearNode ln = new LinearNode(n, pos, ParentPosition.Linear);
+                    ln.Depth = n.Depth + 1;
+                    linear.Next = ln;
+                    n = ln;
+                }
+                else if (grid[n.Pos.row + 1, n.Pos.col].Equals('^'))
+                {
+                    Position pos = new Position(n.Pos.row + 1, n.Pos.col);
+                    ForkNode fn = new ForkNode(linear, pos, ParentPosition.Linear);
+                    fn.Depth = n.Depth + 1;
+                    linear.Next = fn;
+                    n = fn;
+
+                }
+
+            }
+
+            else if (n is ForkNode fork)
+            {
+                //left
+                if (grid[n.Pos.row + 1, n.Pos.col - 1].Equals('.'))
+                {
+                    Position pos = new Position(n.Pos.row + 1, n.Pos.col - 1);
+                    LinearNode ln1 = new LinearNode(n, pos, ParentPosition.Left);
+                    ln1.Depth = n.Depth + 1;
+                    fork.NextLeft = ln1;
+                }
+
+                //right
+                if (grid[n.Pos.row + 1, n.Pos.col + 1].Equals('.'))
+                {
+                    Position pos = new Position(n.Pos.row + 1, n.Pos.col + 1);
+                    LinearNode ln2 = new LinearNode(n, pos, ParentPosition.Right);
+                    ln2.Depth = n.Depth + 1;
+                    fork.NextRight = ln2;
+                }
+
+                if (fork.NextLeft != null)
+                {
+                    n = fork.NextLeft;
+                }
+            }
+        }
+        Console.WriteLine("Counter: {0}", counter);
+    }
+
+}
